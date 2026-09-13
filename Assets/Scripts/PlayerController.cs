2810eeb1a6e21f77b5ee1bdf4c5c9cf78e7e21e8
Stone Yuan <jackyuan110@gmail.com>
2026-09-12 18:16:30 -0700

    first commit

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+public class NewMonoBehaviourScript : MonoBehaviour
+{
+
+    [SerializeField] private float speed = 5f;    
+    [SerializeField] private Transform target;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+}
