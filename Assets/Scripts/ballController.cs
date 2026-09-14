0af5242ff8fc7f9a9cfa79fa308e53fdec660d88
Stone Yuan <jackyuan110@gmail.com>
2026-09-13 18:44:22 -0700

    move player

---
+++ Assets/Scripts/ballController.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class ballController : MonoBehaviour
+{
+    public float speed = 0;
+    private Rigidbody body;
+    private float movementX;
+    private float movementY;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {   
+        body = GetComponent<Rigidbody>();
+    }
+
+    void OnMove(InputValue movementValue)
+    {
+        Vector2 movementVector = movementValue.Get<Vector2>();
+        movementX = movementVector.x; 
+        movementY = movementVector.y; 
+    }
+
+    void FixedUpdate()
+    {
+        Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+        body.AddForce(movement * speed);
+    }
+}
